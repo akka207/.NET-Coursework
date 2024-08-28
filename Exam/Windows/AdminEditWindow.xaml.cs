161e28390d5bc2c0de1c4e8f6863fd72c18364dc
fallenagelcoder <76203302+FallenAngel-coder@users.noreply.github.com>
2024-08-28 10:16:30 +0300

    SettingsManager

--- Exam/Windows/AdminEditWindow.xaml.cs
+++ Exam/Windows/AdminEditWindow.xaml.cs
@@ -23,10 +23,39 @@ namespace Exam.Windows
     public partial class AdminEditWindow : Window
     {
         public Staff xD;
+        private ApplicationSettings _appSettings;
+        private string _windowId = "AdminEditWindow";
         public AdminEditWindow()
         {
             InitializeComponent();
-
+            _appSettings = SettingsManager.LoadSettings();
+            ApplySettings();
+        }
+        private void ApplySettings()
+        {
+            if (_appSettings.Windows.TryGetValue(_windowId, out WindowSettings settings))
+            {
+                this.Width = settings.Width;
+                this.Height = settings.Height;
+                this.Top = settings.Top;
+                this.Left = settings.Left;
+                if (settings.IsMaximized)
+                    this.WindowState = WindowState.Maximized;
+            }
+        }
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            var settings = new WindowSettings
+            {
+                Width = this.Width,
+                Height = this.Height,
+                Top = this.Top,
+                Left = this.Left,
+                IsMaximized = (this.WindowState == WindowState.Maximized)
+            };
+            _appSettings.Windows[_windowId] = settings;
+            SettingsManager.SaveSettings(_appSettings);
         }
         public void SetStaff()
         {
---
+++ Exam/Windows/SettingsManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+namespace Exam.Windows
+{
+    public class ApplicationSettings
+    {
+        public Dictionary<string, WindowSettings> Windows { get; set; } = new Dictionary<string, WindowSettings>();
+    }
+
+    public class WindowSettings
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public bool IsMaximized { get; set; }
+    }
+    public static class SettingsManager
+    {
+        private static readonly string SettingsFilePath = "WindowSettings.json";
+
+        public static ApplicationSettings LoadSettings()
+        {
+            if (File.Exists(SettingsFilePath))
+            {
+                try
+                {
+                    string json = File.ReadAllText(SettingsFilePath);
+                    return JsonConvert.DeserializeObject<ApplicationSettings>(json);
+                }
+                catch (Exception)
+                {
+                    // мяу
+                }
+            }
+            return new ApplicationSettings();
+        }
+
+        public static void SaveSettings(ApplicationSettings settings)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(settings, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(SettingsFilePath, json);
+            }
+            catch (Exception ex)
+            {
+                // мяу
+            }
+        }
+    }
+}
