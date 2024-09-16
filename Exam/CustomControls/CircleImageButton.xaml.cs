30e92c7289bb1d0f9ec035ac543417ab4f25af89
akka207 <alex2007kosh@gmail.com>
2024-09-16 10:45:04 +0300

    Schedule design. NOT FINAL

--- Exam/CustomControls/CircleImageButton.xaml.cs
+++ Exam/CustomControls/CircleImageButton.xaml.cs
@@ -20,18 +20,36 @@ namespace Exam.CustomControls
     /// </summary>
     public partial class CircleImageButton : UserControl
     {
-        #region Attached properties area
-        public string IconPath
-        {
-            get { return (string)GetValue(IconPathProperty); }
-            set { SetValue(IconPathProperty, value); }
-        }
+		#region Attached properties area
+		public string IconPath
+		{
+			get { return (string)GetValue(IconPathProperty); }
+			set { SetValue(IconPathProperty, value); }
+		}
 
-        public static readonly DependencyProperty IconPathProperty =
-            DependencyProperty.Register("IconPath", typeof(string), typeof(CircleImageButton), new UIPropertyMetadata(""));
-        #endregion
+		public static readonly DependencyProperty IconPathProperty =
+			DependencyProperty.Register("IconPath", typeof(string), typeof(CircleImageButton), new UIPropertyMetadata(""));
 
-        public event EventHandler OnClick;
+		public int ImageWidth
+		{
+			get { return (int)GetValue(ImageWidthProperty); }
+			set { SetValue(ImageWidthProperty, value); }
+		}
+
+		public static readonly DependencyProperty ImageWidthProperty =
+			DependencyProperty.Register("ImageWidth", typeof(int), typeof(CircleImageButton), new UIPropertyMetadata(0));
+
+		public int ImageHeight
+		{
+			get { return (int)GetValue(ImageHeightProperty); }
+			set { SetValue(ImageHeightProperty, value); }
+		}
+
+		public static readonly DependencyProperty ImageHeightProperty =
+			DependencyProperty.Register("ImageHeight", typeof(int), typeof(CircleImageButton), new UIPropertyMetadata(0));
+		#endregion
+
+		public event EventHandler OnClick;
 
         public CircleImageButton()
         {
