e9dc77c799ddb15a1b9af3ce3b4a793bb17e0a11
akka207 <alex2007kosh@gmail.com>
2024-09-25 10:50:40 +0300

    Fix year formatting

--- Exam/MenuControls/Schedule/DateTimeToDateConverter.cs
+++ Exam/MenuControls/Schedule/DateTimeToDateConverter.cs
@@ -8,24 +8,25 @@ using System.Windows.Data;
 
 namespace Exam.MenuControls
 {
-    class DateTimeToDateConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is DateTime)
-            {
-                DateTime dateTime = (DateTime)value;
-                return $"{(dateTime.Day < 10 ? "0" : "") + dateTime.Day.ToString()}.{(dateTime.Month < 10 ? "0" : "") + dateTime.Month.ToString()}.{dateTime.Year}";
-            }
-            else
-            {
-                return value;
-            }
-        }
+	class DateTimeToDateConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is DateTime)
+			{
+				DateTime dateTime = (DateTime)value;
+				DateTime now = DateTime.Now;
+				return $"{(dateTime.Day < 10 ? "0" : "") + dateTime.Day.ToString()}.{(dateTime.Month < 10 ? "0" : "") + dateTime.Month.ToString()}{(dateTime.Year != now.Year ? ("." + dateTime.Year) : "")}";
+			}
+			else
+			{
+				return value;
+			}
+		}
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
