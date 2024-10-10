9b71b88e44ce2cb4475996675139ba7c68aa3ee9
ipz243_kov <ipz243_kov@student.ztu.edu.ua>
2024-10-10 20:13:52 +0300

    Profile

--- Exam/MenuControls/PersonnelControl.xaml.cs
+++ Exam/MenuControls/PersonnelControl.xaml.cs
@@ -21,87 +21,98 @@ using System.Windows.Shapes;
 using Exam.Windows;
 namespace Exam.MenuControls
 {
-    /// <summary>
-    /// Interaction logic for PersonnelControl.xaml
-    /// </summary>
-    public partial class PersonnelControl : UserControl, INotifyPropertyChanged
-    {
-        private ObservableCollection<Staff> staffList = null;
-        public ObservableCollection<Staff> StaffList
-        {
-            get
-            {
-                return staffList;
-            }
-            set
-            {
-                if (value != staffList)
-                {
-                    staffList = value;
-                    OnPropertyChanged(nameof(StaffList));
-                }
-            }
-        }
+	/// <summary>
+	/// Interaction logic for PersonnelControl.xaml
+	/// </summary>
+	public partial class PersonnelControl : UserControl, INotifyPropertyChanged
+	{
+		private ObservableCollection<Staff> staffList = null;
+		public ObservableCollection<Staff> StaffList
+		{
+			get
+			{
+				return staffList;
+			}
+			set
+			{
+				if (value != staffList)
+				{
+					staffList = value;
+					OnPropertyChanged(nameof(StaffList));
+				}
+			}
+		}
 
-        public PersonnelControl()
-        {
-            InitializeComponent();
-        }
+		public PersonnelControl()
+		{
+			InitializeComponent();
+		}
 
-        private void control_Loaded(object sender, RoutedEventArgs e)
-        {
-            StaffList = new ObservableCollection<Staff>(DBController.GetAllStaff());
-        }
-
-
+		private void control_Loaded(object sender, RoutedEventArgs e)
+		{
+			StaffList = new ObservableCollection<Staff>(DBController.GetAllStaff());
+		}
 
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private void staff_ListBoxItem_Loaded(object sender, RoutedEventArgs e)
-        {
-            var addEventButton = ((sender as Border).Child as StackPanel).Children[3];
-            var editStaffButton = ((sender as Border).Child as StackPanel).Children[4];
 
 
-            switch(DBController.CurrentStaff.Role.Name)
-            {
-                case "User":
-                    addEventButton.Visibility = Visibility.Hidden;
-                    editStaffButton.Visibility = Visibility.Hidden;
-                    break;
-                case "Manager":
-                    addEventButton.Visibility = Visibility.Visible;
-                    editStaffButton.Visibility = Visibility.Hidden;
-                    break;
-                case "Admin":
-                    addEventButton.Visibility = Visibility.Visible;
-                    editStaffButton.Visibility = Visibility.Visible;
-                    break;
-            }
-        }
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged(string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 
-        private void addEventButton_Click(object sender, RoutedEventArgs e)
-        {
-            AddEventWindow window = new AddEventWindow();
-            window.SelectedStaff = (sender as Button).DataContext as Staff;
-            window.ShowDialog();
-            if(window.SelectedStaff.Id == DBController.CurrentStaff.Id)
-            {
-                DBController.UpdateCurrentStaff();
-            }
-        }
+		private void staff_ListBoxItem_Loaded(object sender, RoutedEventArgs e)
+		{
+			Button addEventButton = null;
+			Button editStaffButton = null;
 
-        private void editStaffButton_Click(object sender, RoutedEventArgs e)
-        {
-            AdminEditWindow window = new AdminEditWindow();
-            window.xD = (sender as Button).DataContext as Staff;
-            window.SetStaff();
-            window.ShowDialog();
-        }
-    }
+			foreach (object element in ((sender as Border).Child as Grid).Children)
+			{
+				if(element is Button && (element as Button).Name == "addEventButton")
+				{
+					addEventButton = element as Button;
+				}
+				else if(element is Button && (element as Button).Name == "editStaffButton")
+				{
+					editStaffButton = element as Button;
+				}
+			}
+
+			switch (DBController.CurrentStaff.Role.Name)
+			{
+				case "User":
+					addEventButton.Visibility = Visibility.Hidden;
+					editStaffButton.Visibility = Visibility.Hidden;
+					break;
+				case "Manager":
+					addEventButton.Visibility = Visibility.Visible;
+					editStaffButton.Visibility = Visibility.Hidden;
+					break;
+				case "Admin":
+					addEventButton.Visibility = Visibility.Visible;
+					editStaffButton.Visibility = Visibility.Visible;
+					break;
+			}
+		}
+
+		private void addEventButton_Click(object sender, RoutedEventArgs e)
+		{
+			AddEventWindow window = new AddEventWindow();
+			window.SelectedStaff = (sender as Button).DataContext as Staff;
+			window.ShowDialog();
+			if (window.SelectedStaff.Id == DBController.CurrentStaff.Id)
+			{
+				DBController.UpdateCurrentStaff();
+			}
+		}
+
+		private void editStaffButton_Click(object sender, RoutedEventArgs e)
+		{
+			AdminEditWindow window = new AdminEditWindow();
+			window.xD = (sender as Button).DataContext as Staff;
+			window.SetStaff();
+			window.ShowDialog();
+		}
+	}
 }
